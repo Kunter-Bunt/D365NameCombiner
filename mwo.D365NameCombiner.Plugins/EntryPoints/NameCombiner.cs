6ee685edf604a401b83f5f3548a96ca587aa537d
Marius Wodtke <marius.wodtke@gmail.com>
2021-01-15 21:03:12 +0100

    NameCombiner

---
+++ mwo.D365NameCombiner.Plugins/EntryPoints/NameCombiner.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xrm.Sdk;
+using mwo.D365NameCombiner.Plugins.Executables;
+using mwo.D365NameCombiner.Plugins.Models;
+using mwo.D365NameCombiner.Plugins.Services;
+using System;
+
+namespace mwo.D365NameCombiner.Plugins.EntryPoints
+{
+    public class NameCombiner : IPlugin
+    {
+        private string UnsecureConfiguration { get; set; }
+        private string SecureConfiguration { get; set; }
+
+        public NameCombiner(string unsecureConfiguration, string secureConfiguration)
+        {
+            UnsecureConfiguration = unsecureConfiguration;
+            SecureConfiguration = secureConfiguration;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = new CRMPluginContext(serviceProvider);
+
+            var attributeService = new AttributeConverterService(context);
+            var expressionService = new ExpressionConverterService(context);
+            var combinerService = new CombinerService(context.PostImage, attributeService, expressionService);
+
+            var executable = new NameCombinationExecutable(combinerService, context);
+            executable.Execute(context.Target, UnsecureConfiguration);
+        }
+    }
+}
---
+++ mwo.D365NameCombiner.Plugins/Executables/NameCombinationExecutable.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using mwo.D365NameCombiner.Plugins.Models;
+using mwo.D365NameCombiner.Plugins.Services;
+using System;
+using System.ServiceModel;
+
+namespace mwo.D365NameCombiner.Plugins.Executables
+{
+    public class NameCombinationExecutable
+    {
+        private CombinerService CombinerService { get; set; }
+        private ICRMContext Context { get; set; }
+
+        private string[] configFields = new string[]
+        {
+            mwo_NameCombination.Fields.mwo_Column,
+            mwo_NameCombination.Fields.mwo_Format,
+            mwo_NameCombination.Fields.mwo_format0,
+            mwo_NameCombination.Fields.mwo_format1,
+            mwo_NameCombination.Fields.mwo_format2,
+            mwo_NameCombination.Fields.mwo_format3,
+            mwo_NameCombination.Fields.mwo_format4,
+            mwo_NameCombination.Fields.mwo_format5,
+            mwo_NameCombination.Fields.mwo_format6,
+            mwo_NameCombination.Fields.mwo_format7,
+            mwo_NameCombination.Fields.mwo_format8,
+            mwo_NameCombination.Fields.mwo_format9
+        };
+
+        public NameCombinationExecutable(CombinerService combinerService, ICRMContext context)
+        {
+            CombinerService = combinerService;
+            Context = context;
+        }
+
+        public void Execute(Entity target, string configString)
+        {
+            var config = GetConfig(configString);
+            if (config == null)
+            {
+                Context.Trace.Trace($"Unable to find Name Combination \"{configString}\", skipping combination.");
+                return;
+            }
+
+            Context.Trace.Trace($"Combining for {config.mwo_Column}");
+            target[config.mwo_Column] = CombinerService.Combine(config.mwo_Format,
+                                                                config.mwo_format0,
+                                                                config.mwo_format1,
+                                                                config.mwo_format2,
+                                                                config.mwo_format3,
+                                                                config.mwo_format4,
+                                                                config.mwo_format5,
+                                                                config.mwo_format6,
+                                                                config.mwo_format7,
+                                                                config.mwo_format8,
+                                                                config.mwo_format9);
+        }
+
+        private mwo_NameCombination GetConfig(string configString)
+        {
+            if (Guid.TryParse(configString, out Guid configId))
+            {
+                try
+                {
+                    var config = Context.OrgService.Retrieve(
+                                                    mwo_NameCombination.EntityLogicalName,
+                                                    configId,
+                                                    new ColumnSet(configFields));
+
+                    return config.ToEntity<mwo_NameCombination>();
+                }
+                catch (FaultException<OrganizationServiceFault> e)
+                {
+                    Context.Trace.Trace($"Unable to retrieve Name Combination \"{configString}\": {e.Message}");
+                }
+            }
+            else 
+                Context.Trace.Trace($"Unable to parse Configuration String: {configString}");
+
+            return null;
+        }
+    }
+}
